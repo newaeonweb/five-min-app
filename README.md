1ff8809c53d2f75cdac0602d0c1746f72ce7ecfd
newaeonweb <fernando@newaeonweb.com.br>
2018-02-16 18:15:57 -0200

    init universal project

--- README.md
+++ README.md
@@ -25,3 +25,17 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+npm update -g @angular/cli
+
+ng new five-min-app --style=scss --routing
+
+ng generate universal ngu-app-shell
+
+yarn add @angular/platform-server
+
+ng generate module pages/home --routing
+
+ng generate component pages/home
+
+ng generate app-shell my-loading-shell  --universal-app=ngu-app-shell  --route=home 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
+import { HeaderComponent } from './layout/header/header.component';
+import { HomeModule } from './pages/home/home.module';
+import { HomeRoutingModule } from './pages/home/home-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    RouterModule,
+    HomeRoutingModule,
+    HomeModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
